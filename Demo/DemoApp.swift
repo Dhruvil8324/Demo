b1a5717510304e4c5f9c22eae413e3dd697731c1
Dhruvil Patel <dhruvil.8302@gmail.com>
2024-05-09 13:50:48 -0700

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by user254195 on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
